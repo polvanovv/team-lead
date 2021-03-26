388dc46f6203cb8706fbe43be237efc622bcbe18
Polvanov Igor <polvanovv@gmail.com>
2021-03-26 11:25:44 +0600

    junior result generator added

---
+++ src/Model/TeamLead/Entity/Service/JuniorResultGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\TeamLead\Entity\Service;
+
+/**
+ * Class JuniorChecker
+ *
+ * @package App\Model\TeamLead\Entity\Service
+ * @author Polvanov Igor <polvanovv@gmail.com>
+ */
+class JuniorResultGenerator
+{
+    /**
+     * @return int
+     */
+    public static function generateResult(): int
+    {
+        return rand(0, 1);
+    }
+
+    /**
+     * @return int
+     */
+    public static function successResult(): int
+    {
+        return 1;
+    }
+
+    /**
+     * @return int
+     */
+    public static function failureResult(): int
+    {
+        return 0;
+    }
+}
