912b8f11e32acd239342d0ccc16c872618ac085b
Polvanov Igor <polvanovv@gmail.com>
2021-03-26 13:56:15 +0600

    remove unused code

--- src/App.php
+++ src/App.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Entity\HumanResources\HumanResources;
-use App\Entity\Manager\Manager;
-use App\Entity\TeamLead\Event\EventChanel;
-use App\Entity\TeamLead\Event\Publisher;
-
 /**
  * Class App
  *
@@ -17,20 +12,5 @@ use App\Entity\TeamLead\Event\Publisher;
  */
 class App
 {
-    public function run()
-    {
-        $eventChanel = new EventChanel();
 
-        $praisePublisher    = new Publisher('praise', $eventChanel);
-        $reprimandPublisher = new Publisher('reprimand', $eventChanel);
-
-        $manager = new Manager();
-        $hr      = new HumanResources();
-
-        $eventChanel->subscribe('praise', $manager);
-        $eventChanel->subscribe('reprimand', $hr);
-
-        $praisePublisher->publish(5);
-        $reprimandPublisher->publish(7);
-    }
 }
