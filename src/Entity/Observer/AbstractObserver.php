43fa838880257e3bfdce09c6369273f6bec053d2
Polvanov Igor <polvanovv@gmail.com>
2021-03-28 13:22:49 +0600

    added coments

--- src/Entity/Observer/AbstractObserver.php
+++ src/Entity/Observer/AbstractObserver.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 
 namespace App\Entity\Observer;
 
-
+/**
+ * Class AbstractObserver
+ *
+ * @package App\Entity\Observer
+ * @author Polvanov Igor <igor.polvanov@sibers.com>
+ */
 abstract class AbstractObserver implements \SplObserver
 {
     /**
